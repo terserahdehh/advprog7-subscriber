726335daccff70d59cf49378b7d19060347cfc56
terserahdehh <serafina.nala@gmail.com>
2025-03-26 19:59:47 +0700

    Implement receive_notification function in Notification service.

--- src/service/notification.rs
+++ src/service/notification.rs
@@ -54,7 +54,7 @@ impl NotificationService {
             ))
         }
     }
-    
+
     pub fn unsubscribe(product_type: &str) -> Result<SubscriberRequest> {
         let product_type_clone = String::from(product_type);
         return thread::spawn(move || Self::unsubscribe_request(product_type_clone))
@@ -90,6 +90,11 @@ impl NotificationService {
                 e.to_string()
             ))
         }
+    }
+    
+    pub fn receive_notification(payload: Notification) -> Result<Notification> {
+        let subscriber_request: Notification = NotificationRepository::add(payload);
+        return Ok(subscriber_request);
     }
 }
 
